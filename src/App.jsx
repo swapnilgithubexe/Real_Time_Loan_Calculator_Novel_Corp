047c7c58b5e4ba791732c8d2c3a9faddca8c0061
swapnilgithubexe <swapnildutta2002@gmail.com>
2025-05-06 03:47:25 +0530

    feat(navbar), added the context files, created basic folder structure, installed dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route element={<Navbar />}></Route>
+        <Route path="*" element />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
